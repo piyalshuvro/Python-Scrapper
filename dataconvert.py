1d8f9f53f18f4e6ddfa8f20b0a761f21e1a3c9d0
piyalshuvro <piyalshuvro@users.noreply.github.com>
2017-12-05 11:19:39 +1100

    5/12/2017

--- dataconvert.py
+++ dataconvert.py
@@ -1,37 +1,44 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import csv
+import html5lib
+
+for number in range(1,208):
+	for i, df in enumerate(pd.read_html("GpResult(%s).xls" % number,header = 0)):
+		df.drop(df.index[0])
+		df.to_csv('myfile_%s.csv' % number)
 
 
 
-for i, df in enumerate(pd.read_html("GpResult (12).xls")):
-    df.to_csv('myfile_%s.csv' % i)
+# for i, df in enumerate(pd.read_html("GpResult (12).xls",header = 0)):
+# 	df.drop(df.index[0])
+# 	df.to_csv('myfile_%s.csv' % 2)
+
+for number in range(1,208):
+	with open("myfile_%s.csv" %number, "rb") as fp_in, open("newfile_%s.csv" %number, "wb") as fp_out:
+		reader = csv.reader(fp_in, delimiter=",")
+		writer = csv.writer(fp_out, delimiter=",")
+		for row in reader:
+			del row[0]
+			writer.writerow(row)
 
 
+		# reader = csv.reader(fp_in, delimiter=",")
+  #   	writer = csv.writer(fp_out, delimiter=",")
+  #   	for row in reader:
+  #   		del row[0]
+  #   		writer.writerow(row)
+    	
+
+    	
 
 
+# with open("myfile_2.csv", "rb") as fp_in, open("newfile.csv", "wb") as fp_out:
+    # reader = csv.reader(fp_in, delimiter=",")
+    # writer = csv.writer(fp_out, delimiter=",")
+    # for row in reader:
+    #     del row[0]
+    #     writer.writerow(row)
 
-
-
-
-
-# soup = BeautifulSoup("GpResult (12).xls")
-# tables = soup.findChildren('table')
-# print(tables)
-
-# my_table = tables[0]
-# rows = my_table.findChildren(['th', 'tr'])
-# for row in rows:
-# 	cells = row.findChildren('td')
-# 	for cell in cells:
-# 		value = cell.string
-# 		print(value)
-
-
-# results = pd.read_html("GpResult (12).xls")
-# with open('filename.csv', 'wb') as myfile:
-#     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-#     wr.writerow(results)
-	
 
 
--- scrapper.py
+++ scrapper.py
@@ -3,13 +3,14 @@ from selenium.webdriver.support.ui import Select;
 from selenium.webdriver.support import expected_conditions as EC;
 from selenium.webdriver.support.ui import WebDriverWait;
 from selenium.webdriver.common.by import By;
+from selenium.common.exceptions import NoSuchElementException
 import time;
 
 
+data_list = []
 
-
-driver = webdriver.Chrome()
-driver.get("http://wbsec.gov.in/(S(o11dyg555mfwcn450q0abwu4))/DetailedResult/Detailed_gp.aspx")
+driver = webdriver.Firefox()
+driver.get("http://www.wbsec.gov.in/(S(tqjmgl55rl2vw1bnrtih2r45))/DetailedResult/Detailed_gp_2013.aspx")
 
 
 # first loop for district
@@ -18,7 +19,8 @@ districtSelect = Select(driver.find_element_by_name('ddldistrict'))
 districtOption = districtSelect.options
 
 
-for districtValue in range(1,len(districtOption)):
+
+for districtValue in range(5,len(districtOption)):
 	districtSelect = Select(driver.find_element_by_name('ddldistrict'))
 	
 	districtSelect.select_by_index(districtValue)
@@ -40,13 +42,27 @@ for districtValue in range(1,len(districtOption)):
 		gpOption = gpSelect.options
 
 		for gpValue in range(1,len(gpOption)):
-		
-	                gpSelect = Select(driver.find_element_by_name('ddlgp'))
-	
-			gpSelect.select_by_index(gpValue)
+			try:
+				gpSelect = Select(driver.find_element_by_name('ddlgp'))
+				gpSelect.select_by_index(gpValue)
 
-    			print_button = WebDriverWait(driver, 5).until(
-        			EC.presence_of_element_located((By.XPATH, '//input[@id = "btnprint"]'))
-    			)
-			print_button.click()
-			driver.back()
+				print_button = WebDriverWait(driver, 30).until(
+	    			EC.presence_of_element_located((By.XPATH, '//input[@id = "btnprint"]'))
+				)
+				print_button.click()
+				districtSelect = Select(driver.find_element_by_name('ddldistrict'))
+				districtOption = districtSelect.options
+				panchayatSelect = Select(driver.find_element_by_name('ddlblock'))
+				panchayatOption = panchayatSelect.options;
+				gpSelect = Select(driver.find_element_by_name('ddlgp'))
+				gpOption = gpSelect.options
+				with open("test.txt", "a") as myfile:
+					myfile.write(districtOption[districtValue].text + ' ' + panchayatOption[panchayatValue].text + ' ' + gpOption[gpValue].text + '\n')
+
+				time.sleep(5)
+			except NoSuchElementException:
+				driver.save_screenshot("filename2.png")
+				driver.back()
+				
+			
+			
